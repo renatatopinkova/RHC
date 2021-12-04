deac1dffb43089ab44ed11b9f1251a667a8a3dd1
renatatopinkova <73588752+renatatopinkova@users.noreply.github.com>
2021-12-04 00:52:33 +0000

    added new data collection

---
+++ 2_scrape_new.R
@@ -0,0 +1,50 @@
+
+# Load libraries ----------------------------------------------------------
+
+library(rvest)
+library(dplyr)
+
+
+
+# Extract -----------------------------------------------------------------
+
+# read the page
+page <- read_html("https://metro.co.uk/rush-hour-crush/?ico=rhc_banner_home/home")
+
+# extract text and author
+text <- html_text(html_elements(page, ".metro-rush-hour-crush p"))
+author <- html_text(html_elements(page, ".metro-rush-hour-crush h4"))
+
+# bind it to dataframe
+df <- data.frame(text = text, author = author)
+
+# add date of collection
+df$date <- Sys.time()
+
+
+
+# Combine -----------------------------------------------------------------
+
+path <- "D:/Nextcloud/_clanky/UCL_Rush_hour_crush/"
+
+# load old dataframe
+df_old <- readRDS(paste0(path, "RHC_dataframe"))
+
+
+# find what's new
+new <- df %>%
+       group_by(author, text) %>%
+         subset(!(text%in%df_old$text))
+
+# bind it to a new one
+df_updated <- rbind(new, df_old)
+
+
+
+# Save --------------------------------------------------------------------
+
+
+# save results
+saveRDS(df_updated, paste0(path, "RHC_dataframe"))
+write.csv(df_updated, paste0(path, "RHC_dataframe.csv"), row.names = F)
+
